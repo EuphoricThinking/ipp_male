782cd5b5e735c744c8810c11763243844f063048
Agata <amomot@bio-prodict.nl>
2022-03-30 01:29:35 +0200

    Queue interface seems to work correct

--- main.c
+++ main.c
@@ -6,6 +6,14 @@ int main() {
 	push(q, 2);
 	push(q, 6);
 	push(q, 12);
-	printf("%lld\n", top(q));
+	//printf("%lld\n", top(q));
+
+	while (!isEmpty(q)) {
+		unsigned long long res_top = top(q);
+		unsigned long long res_pop = pop(q);
+		printf("%lld, %lld\n", res_top, res_pop);
+	}
+
+	deleteQueue(q);
 	return 0;
 }
