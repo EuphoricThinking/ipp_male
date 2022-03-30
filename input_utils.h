c6b22e0833afa9e61c37466526c00e7181040339
Agata <amomot@bio-prodict.nl>
2022-03-30 11:53:14 +0200

    Going to write input reading

---
+++ input_utils.h
@@ -0,0 +1,7 @@
+#ifndef INPUT_UTILS_H
+#define INPUT_UTILS_H
+#include "labirynth_struct.h"
+
+Labirynth read_input();
+
+#endif
---
+++ labirynth_struct.h
@@ -0,0 +1,26 @@
+#ifndef LABIRYNTH_STRUCT_H
+#define LABIRYNTH_STRUCT_H
+
+typedef struct Cube {
+	size_t* coordinates;
+	size_t num_dimension;
+} Cube;
+
+typedef struct Labirynth {
+	size_t num_dimensions;
+	size_t* dimension_sizes;
+	size_t* start_coordinates;
+	size_t* end_coordinates;
+} Labirynth;
+
+typedef struct Index_availability {
+	unsigned int is_available: 1;
+} Index_availability;
+
+typedef struct Availability_array {
+	struct Index_availability* available_indexes;
+} Availability_array;
+
+#endif
+
+
