c07fc14b9c73b3339989a45663378951deb9ce7b
Agata <amomot@bio-prodict.nl>
2022-03-31 03:07:32 +0200

    Written convert hex helper conversion'

--- bit_lab_utils.c
+++ bit_lab_utils.c
@@ -1,4 +1,22 @@
 #include "bit_lab_utils.h"
+#include <string.h>
+
+#define A_VAL 10
+#define B_VAL 11
+#define C_VAL 12
+#define D_VAL 13
+#define E_VAL 14
+#define F_VAL 15
+
+int return_hex_val(char sign) {
+	if (sign == 'A' || sign == 'a') return A_VAL;
+	else if (sign == 'B' || sign == 'b') return B_VAL;
+	else if (sign == 'C' || sign == 'c') return C_VAL;
+	else if (sign == 'D' || sign == 'd') return D_VAL;
+	else if (sign == 'E' || sign == 'e') return E_VAL;
+	else if (sign == 'F' || sign == 'f') return F_VAL;
+	else return sign - '0';
+}
 
 Bitmap* convert_to_hex_bitmap(const char* hex, size_t length);
 
