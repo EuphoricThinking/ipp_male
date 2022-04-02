25b75e9e0e0dc99040815ae8b79872f7172dc6d9
Agata <amomot@bio-prodict.nl>
2022-04-02 11:29:40 +0200

    Begun working on bash script

--- test.sh
+++ test.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
+GREEN="\033[0;32m"
 RED="\033[0;31m"
+NOCOL="\033[0m"
 
 function correct_result {
-	printf "${RED}WRONG\n"
+	printf "${GREEN}OK\n${NOCOL}"
+}
+
+function wrong_result {
+	printf "${RED}WRONG\n${NOCOL}"
+}
+
+function test_args {
+	if [ "$#" -ne 2 ]; then
+		echo "Too few arguments"
+
+		return 1;
+	fi
 }
 
 correct_result
+wrong_result
+test_args
